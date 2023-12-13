c240d1f8e02c7ccfd5964c57a249271cd281dc70
LifeIsOne <didtmdgh258@gmail.com>
2023-12-13 12:40:53 +0900

    statci 공부ì

---
+++ src/ex02/IdentityEx01.java
@@ -0,0 +1,20 @@
+package ex02;
+
+/**
+ * 변수 작성법
+ * 1. 숫자를 먼저 사용하지 않기
+ * 2. 특수문자를 사용하지 않기
+ * 3. CamelCase 사용하기
+ */
+public class IdentityEx01 {
+    public static void main(String[] args) {
+        //int class;        keyworkd 사용 금지
+        int user_money;     // 되지만... 절대 사용 안함
+        int userMoney;      // Camel case 낙타 표기법
+        //int abc#;         특수문자 사용하지 말기
+        //int abc$$;
+        int method;
+        // int 1stMoney     숫자 X
+
+    }
+}
