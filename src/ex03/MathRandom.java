0ef27cf4687b24372c4e0e50f296208d7f3665bf
LifeIsOne <didtmdgh258@gmail.com>
2023-12-14 12:46:07 +0900

    For문

---
+++ src/ex03/MathRandom.java
@@ -0,0 +1,8 @@
+package ex03;
+
+public class MathRandom {
+    public static void main(String[] args) {
+        //System.out.println(Math.random());
+        System.out.println((int)(Math.random()*10));
+    }
+}
