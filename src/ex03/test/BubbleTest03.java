0be7a2233a4e8f73088057aa81cf4b46614ad838
LifeIsOne <didtmdgh258@gmail.com>
2023-12-18 11:00:57 +0900

    이진트리 연습중

--- src/ex03/test/BubbleTest03.java
+++ src/ex03/test/BubbleTest03.java
@@ -1,4 +1,4 @@
-package ex03.BubbleSort;
+package ex03.test;
 // 스왑하기, temp
 public class BubbleTest03 {
     public static void main(String[] args) {
--- src/ex03/test/test01.java
+++ src/ex03/test/test01.java
@@ -1,4 +1,4 @@
-package ex03.BubbleSort;
+package ex03.test;
 
 public class test01 {
 }
