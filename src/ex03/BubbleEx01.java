87e6fc6fd72318fdfa33f3783f14eed9bc177462
LifeIsOne <didtmdgh258@gmail.com>
2023-12-18 10:37:46 +0900

    선택정렬

--- src/ex03/BubbleEx01.java
+++ src/ex03/BubbleEx01.java
@@ -8,7 +8,7 @@ public class BubbleEx01 {
 
         for (int loop = 1; loop < N; loop++) {
             for (int i = 0; i < N - loop; i++) {
-                if (arr[i] > arr[i + 1]) {
+                if(arr[i] > arr[i + 1]) {
                     temp = arr[i];
                     arr[i] = arr[i + 1];
                     arr[i + 1] = temp;
