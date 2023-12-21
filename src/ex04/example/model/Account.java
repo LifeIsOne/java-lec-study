ac868d70a376a1a82aab1fcda8bc419e51179642
LifeIsOne <didtmdgh258@gmail.com>
2023-12-21 12:02:22 +0900

    계좌이체 절차적 테스트 완료

--- src/ex04/example/model/Account.java
+++ src/ex04/example/model/Account.java
@@ -1,9 +1,9 @@
 package ex04.example.model;
 
 public class Account {
-    final int id;   // final : 선언할 떄 반드시 초기화 돼야 함.
-    long balance;   //
-    int userId;     // 1
+    public final int id;   // final : 선언할 떄 반드시 초기화 돼야 함.
+    public long balance;   //
+    public int userId;     // 1
 
     public Account(int id, long balance, int userId) {
         this.id = id;
