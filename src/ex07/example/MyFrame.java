b9736de8fa066a9a65878bf73b760381ddc70c05
LifeIsOne <didtmdgh258@gmail.com>
2023-12-26 15:43:29 +0900

    람다식

---
+++ src/ex07/example/MyFrame.java
@@ -0,0 +1,15 @@
+package ex07.example;
+
+import javax.swing.*;
+
+class BasicFrame{
+
+}
+
+public class MyFrame {
+    public static void main(String[] args) {
+        JFrame jf = new JFrame();
+        jf.setSize(300, 500);       // 사이즈 정하기
+        jf.setVisible(true);                    // 보여주기
+    }
+}
