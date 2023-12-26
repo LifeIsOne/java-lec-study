08704eb910f037cbe9069926faec2eca9995c133
LifeIsOne <didtmdgh258@gmail.com>
2023-12-26 17:08:26 +0900

    JFrame 공부

--- src/ex07/example/MyFrame.java
+++ src/ex07/example/MyFrame.java
@@ -1,15 +1,39 @@
 package ex07.example;
 
 import javax.swing.*;
+import java.awt.*;
 
-class BasicFrame{
-
-}
 
 public class MyFrame {
+
+    static int num = 1;
+
     public static void main(String[] args) {
         JFrame jf = new JFrame();
+        //jf.setLayout(new FlowLayout());
         jf.setSize(300, 500);       // 사이즈 정하기
+
+        JButton btn1 = new JButton("더하기");
+        JButton btn2 = new JButton("빼기");
+        JLabel la1 = new JLabel(num+"");
+
+        jf.add("South",btn1);
+        jf.add("North",btn2);
+        jf.add("Center", la1);
+
+        btn1.addActionListener(e -> {
+            num ++;
+            la1.setText(num + "");
+        });
+        btn2.addActionListener(e -> {
+            num --;
+            if (num < 0){
+                num = 0;
+            }
+            la1.setText(num + "");
+        });
+
         jf.setVisible(true);                    // 보여주기
+
     }
 }
