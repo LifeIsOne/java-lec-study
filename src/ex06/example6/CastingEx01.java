07b485e43e4736ae9497b63c52959a755590e2b6
LifeIsOne <didtmdgh258@gmail.com>
2023-12-26 10:12:48 +0900

    다향성

---
+++ src/ex06/example6/CastingEx01.java
@@ -0,0 +1,28 @@
+package ex06.example6;
+
+class Protoss{
+    public void attack(){}
+}
+
+class Zealot extends Protoss{
+    public void attack(){
+        System.out.println("질럿이 공격.");
+    }
+}
+
+class Dragoon extends Protoss{
+    public void attack(){
+        System.out.println("드라군 공격.");
+    }
+}
+
+public class CastingEx01 {
+
+    public static void start(Protoss p){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
