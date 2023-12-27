3eca08ab6b3c495ceb10d19ebd7cc3f0270f5ea9
LifeIsOne <didtmdgh258@gmail.com>
2023-12-27 11:45:17 +0900

    버거 앱

--- src/ex06/example7/LotteApp.java
+++ src/ex06/example7/LotteApp.java
@@ -2,6 +2,14 @@ package ex06.example7;
 
 public class LotteApp {
     public static void main(String[] args) {
-
+        Burger b1 = new Burger("기본버거", 1900);
+        ShrimpBurger b2 = new ShrimpBurger("새우버거", 2000, "새우");
+        Coke c1 = new Coke("콜라", 1000);
+        System.out.println();
+        BurgerSet set1 = new BurgerSet(  new Burger("기본버거", 1900),
+                                        new Coke("콜라", 1000));
+//        System.out.println(set1.getBurger().getPrice());
+//        System.out.println(set1.getCoke().getPrice());
+        System.out.println( set1.getTotalPrice() + "원 입니다.");
     }
 }
